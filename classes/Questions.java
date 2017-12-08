2f484b2e2d75d9e2c9060d7423fb84923aacccde
Heuster <32402568+Heuster@users.noreply.github.com>
2017-12-08 14:03:43 -0800

    12/8

---
+++ classes/Questions.java
@@ -0,0 +1,67 @@
+
+/**
+ * Write a description of class Questions here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Questions
+{
+    //1. How many bits does it take to represent the values from 0 to 255?
+    // 8 bits
+    //2. How many bytes does it take to represent a color in the RBG color model
+    // 3 bytes
+    //3. How many pixels are in a picture that is 640 pixels wide and 480 pixels high?
+    // 307200
+    
+    //1. How can you make pink?
+    // [r=255, g=117, b=255]
+    //2. How can you make yellow?
+    // [r=255, g=255, b=0]
+    //3. How can you make purple?
+    // [r=255, g=0, b=255]
+    //4. How can you make white?
+    // [r=255, g=255, b=255]
+    //5. How can you make dark gray?
+    // [r=70, g=70, b=70]
+    
+    //1. What is the row index for the top left corner of the picture?
+    // 0
+    //2. What is the column index for the top left corner of the picture?
+    // 0
+    //3. The width of this picture is 640. What is the right most column index?
+    // 639
+    //4. The height of this picture is 480. What is the bottom most row index?
+    // 479
+    //5. Does the row index increase from left to right or top to bottom?
+    // Top to Bottom
+    //6. Does the column index increase from left to right or top to bottom?
+    // Left to Right
+    //7. Set the zoom to 500%. Can you see squares of color? This is called pixelation. Pixelation means
+    //displaying a picture so magnified that the individual pixels look like small squares.
+
+    //1. Open Picture.java and look for the method getPixels2D. Is it there?
+    // Yes
+    //2. Open SimplePicture.java and look for the method getPixels2D. Is it there?
+    // Yes
+    //3. Does the following code compile?
+    //DigitalPicture p = new DigitalPicture();
+    // Yes
+    //4. Assuming that a no-argument constructor exists for SimplePicture, would the following
+    //code compile?
+    //DigitalPicture p = new SimplePicture();
+    // Yes
+    //5. Assuming that a no-argument constructor exists for Picture, does the following code
+    //compile?
+    //DigitalPicture p = new Picture();
+    // Yes
+    //6. Assuming that a no-argument constructor exists for Picture, does the following code
+    //compile?
+    //SimplePicture p = new Picture();
+    // Yes
+    //7. Assuming that a no-argument constructor exists for SimplePicture, does the following
+    //code compile?
+    //Picture p = new SimplePicture();
+    // Yes
+    
+}
